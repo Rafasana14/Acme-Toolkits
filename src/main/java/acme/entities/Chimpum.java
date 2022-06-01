9770e0fff9842bb6eddeae9c39f8138e211c88da
Rafasana14 <rafasana9@gmail.com>
2022-06-01 19:52:26 +0200

    Primer commit

---
+++ src/main/java/acme/entities/Chimpum.java
@@ -0,0 +1,65 @@
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Chimpum extends AbstractEntity{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4827750132244297758L;
+	
+	
+	//Attributes
+	
+	//@Column(unique = true)
+	//@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	protected String code;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date 				creationMoment;
+	
+	@NotBlank
+	@Length(min=1,max = 100)
+	protected String title;
+	
+	@NotBlank
+	@Length(min=1,max=255)
+	protected String description;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date 				startDate;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date 				endDate;
+	
+	@NotNull
+	@Valid
+	protected Money 			budget;
+	
+	@URL
+	protected String 			moreInfo;
+	
+
+}
